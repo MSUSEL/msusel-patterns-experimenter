ef98ffe7e6db579253746d5d86a0ee59c35d1a2f
Isaac Griffith <isaacgriffith@gmail.com>
2021-10-13 23:59:35 -0600

    updated

--- src/main/groovy/edu/montana/gsoc/msusel/arc/impl/patextract/PatternExtractorCommand.groovy
+++ src/main/groovy/edu/montana/gsoc/msusel/arc/impl/patextract/PatternExtractorCommand.groovy
@@ -53,7 +53,7 @@ class PatternExtractorCommand extends SecondaryAnalysisCommand {
     void execute(ArcContext context) {
         String resultsFile = context.getArcProperty(PatternExtractorConstants.RESULTS_FILE)
         String base = context.getArcProperty(PatternExtractorConstants.BASE_DIR)
-        File baseDir = new File(context.getArcProperty("arc.base.dir"), base)
+        File baseDir = new File(Paths.get(context.getArcProperty("arc.base.dir")).toAbsolutePath().toFile(), base)
 
         def data = new XmlSlurper().parseText(new File(resultsFile).text)
 
