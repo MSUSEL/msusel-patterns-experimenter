35e93cbb4a3b7668b305ad30972c847334c990f0
Isaac Griffith <isaacgriffith@gmail.com>
2021-10-26 07:36:42 -0600

    updated

--- src/main/groovy/edu/montana/gsoc/msusel/arc/app/runner/verification/VerificationInjectorDirector.groovy
+++ src/main/groovy/edu/montana/gsoc/msusel/arc/app/runner/verification/VerificationInjectorDirector.groovy
@@ -60,6 +60,7 @@ class VerificationInjectorDirector {
             file.readLines().each { line ->
                 String[] params = line.split(",")
                 SourceInjector injector = InjectorFactory.instance.createInjector(inst, "grime", params[0])
+                log.info "Injector Params:\n${Arrays.copyOfRange(params, 1, params.length - 1)}"
                 injector.inject(Arrays.copyOfRange(params, 1, params.length - 1))
             }
 
